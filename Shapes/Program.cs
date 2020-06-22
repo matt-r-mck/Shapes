19cb65c1a29ca9af6e5920976b9409eca5fb4c0d
matt-r-mck <matt.r.mck@gmail.com>
2020-06-22 15:57:12 -0400

    Add project files.

---
+++ Shapes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shapes {
+    class Program {
+        static void Main(string[] args) {
+
+            var quad1 = new Quadrilateral();
+            quad1.FindPerimeter(10, 10, 10, 10);
+            Console.WriteLine($"{quad1.Perimeter}");
+
+        }
+    }
+}
---
+++ Shapes/Quadrilateral.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes {
+    public class Quadrilateral {
+
+        //public int Leg1 { get; set; }
+        //public int Leg2 { get; set; }
+        //public int Leg3 { get; set; }
+        //public int Leg4 { get; set; }
+        public int Perimeter { get; set; }
+
+        public Quadrilateral() {
+
+        }
+
+        public int FindPerimeter(int Leg1, int Leg2, int Leg3, int Leg4) {
+            Perimeter = (Leg1 + Leg2 + Leg3 + Leg4);
+            return Perimeter;
+        }
+
+    }
+}
